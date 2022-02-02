2e18af3bc6829e76cebe804ee5367eae19ded998
Charlie Hebert <charlie.hebert92@gmail.com>
2022-02-02 15:58:13 +0000

    Remove lorem ipsum

--- app/routes/blog/posts/index.tsx
+++ app/routes/blog/posts/index.tsx
@@ -27,16 +27,6 @@ export default function Posts() {
           </li>
         ))}
       </ul>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-        sagittis ligula in purus iaculis, non semper dui euismod. Pellentesque
-        luctus posuere augue, a tincidunt enim eleifend id. Nunc at ligula sed
-        libero placerat vehicula. In lobortis, erat ut elementum viverra, sem
-        arcu consequat libero, non auctor risus arcu nec dui. Sed tempus feugiat
-        nibh sed efficitur. Donec tristique orci metus, at congue eros gravida
-        id. Mauris ultrices accumsan sapien non blandit. Integer in sem libero.
-        Etiam a viverra nunc, ut.
-      </p>
     </LimitMaxWidth>
   );
 }
